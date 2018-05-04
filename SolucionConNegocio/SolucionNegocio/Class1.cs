8ea1b0a5061947fc22faae6da4327cafd57a024e
Pedro <38480397+ipeterlow@users.noreply.github.com>
2018-05-03 22:24:08 -0300

    Clase validacion

---
+++ SolucionConNegocio/SolucionNegocio/Class1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolucionNegocio
+{
+    public class Persona
+    {
+
+        private DateTime _fechaNacimineto;
+
+        private string _digitoVerificador;
+
+        private long _rut;
+
+        private string _nombre;
+
+
+        public long Rut
+        {
+            get { return _rut; }
+            set
+            {
+                if (value <= 0 || value >= 100000000)
+                {
+                    throw new ArgumentException("El rut no se encuetra en el rango");
+                }
+
+
+                _rut = value;
+            }
+        }
+
+
+        public string DigitoVerificador
+        {
+            get { return _digitoVerificador; }
+            set
+            {
+                if (value == string.Empty)
+                {
+                    throw new ArgumentException("No se aceptan campos vacios");
+                }
+                else
+                {
+                    _digitoVerificador = value;
+                }
+
+            }
+        }
+
+
+        public DateTime FechaNacimineto
+        {
+            get { return _fechaNacimineto; }
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("No se puede ingresar una fecha mayor a la que hoy");
+                }
+                else
+                {
+                    _fechaNacimineto = value;
+                }
+
+
+                _fechaNacimineto = value;
+            }
+        }
+
+
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if (value == string.Empty)
+                {
+                    throw new ArgumentException("No se puede ingresar un nombre vacio");
+                }
+                else if (value.Trim().Length < 2)
+                {
+                    throw new ArgumentException("El nombre debe contener mas de dos letras");
+                }
+
+                else
+                {
+                    _nombre = value;
+                }
+            }
+        }
+
+
+        public Persona(string nombre, long rut,string dv,DateTime fechaNacimiento)
+        {
+            Nombre = nombre;
+            Rut = rut;
+            DigitoVerificador = dv;
+            FechaNacimineto = fechaNacimiento;
+        }
+
+      
+    }
+
+
+
+}
+
---
+++ SolucionConNegocio/SolucionPresentacion/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SolucionNegocio;
+
+namespace SolucionPresentacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string nombre = txtNombre.Text;
+                long rut = long.Parse(txtRut.Text);
+                DateTime fechaNacimiento = dpFechaNacimiento.SelectedDate.Value;
+                string dv = txtDigitoVerificador.Text;
+
+                Persona p = new Persona(nombre, rut, dv, fechaNacimiento);
+                MessageBox.Show("se ha ingresado una persona");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
